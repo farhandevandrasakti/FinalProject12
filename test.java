340b96572257a8f456da2868aaf6f935c812fc28
mangeaksir <radiantrafie@gmail.com>
2025-10-02 14:02:18 +0700

    Add greeting message to main method in test class

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+        System.out.println("halo sam piye kabare? aku rafi");
     }
 }
