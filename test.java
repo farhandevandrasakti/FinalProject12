6c88f684b11702032fe8cee67d499e364396f302
lorddanu <ilhamdanu010@gmail.com>
2025-10-05 18:45:56 +0700

    percobaan ke 3

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("halo sam piye kabare? aku rafi");
+        System.out.println("halo admin saya penyusup nama saya ilham!! hehe");
     }
 }
